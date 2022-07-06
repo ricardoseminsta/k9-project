d32836d236324327974853e7210e82e3579b6bcb
Ricardo Emanuel Ferreira Silva <ricardo.emanuel66@gmail.com>
2022-07-06 09:35:05 -0300

    commit final project

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,6 +1,21 @@
 import { Request, Response } from "express";
+import { Pet } from "../models/pet";
+import { createMenuObject } from "../helpers/createMenuObject";
+import { request } from "http";
 
 export const search = (req: Request, res: Response) => {
-    //res.render('pages/search',)
+    let query = req.query.q as string;
+    let list = Pet.getFromName(query);
+
+    if(!query) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    });
 };
 
